12381995cd7a87d70c85cfdcf4ca5c0cc2a18b72
ionicaionut <ionicaionut@gmail.com>
2021-02-11 18:30:38 +0200

    update

--- AIS_ERP_Software/AIS_ERP/views.py
+++ AIS_ERP_Software/AIS_ERP/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def main(request):
-    return HttpResponse(" You're at the AIS_ERP main.")
+def index(request):
+    return HttpResponse(" You're at the AIS_ERP Site.")
 
